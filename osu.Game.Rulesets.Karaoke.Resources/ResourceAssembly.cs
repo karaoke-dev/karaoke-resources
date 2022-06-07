2cdfa4cef5e0cc52a0db3e43053e114779ef11c2
andy840119 <andy840119@gmail.com>
2022-06-07 23:34:47 +0800

    Add the initial project.

---
+++ osu.Game.Rulesets.Karaoke.Resources/ResourceAssembly.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+namespace osu.Game.Rulesets.Karaoke.Resources
+{
+    public static class OsuResources
+    {
+        public static Assembly ResourceAssembly => typeof(OsuResources).Assembly;
+    }
+}
